3ed3490611b0309d3e89bffe1dcb74d50076a951
Whitenoizzz <lovey242001@gmail.com>
2023-12-30 09:21:24 +0530

    second commit

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -12,8 +12,9 @@ const auth = async (req, res, next) => {
 
   try {
     const payload = jwt.verify(token, process.env.JWT_SECRET)
+    const testUser = payload.userId === '658e5eb4d5a70f1950dd49f1'
     // attach the user to the job routes
-    req.user = { userId: payload.userId, name: payload.name }
+    req.user = { userId: payload.userId,testUser }
     next()
   } catch (error) {
     throw new UnauthenticatedError('Authentication invalid')
